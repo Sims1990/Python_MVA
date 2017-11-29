147da84a4961d562a696d276108b2265d12099c8
Malcolm Sims <Sims1990@users.noreply.github.com>
2017-11-29 04:58:10 -0600

    udpated to be ran outside of docker on port 0.0.0.0

--- Flask/init.py
+++ Flask/init.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'Test'
-	#return render_template('launch.html')
+    return render_template('launch.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
